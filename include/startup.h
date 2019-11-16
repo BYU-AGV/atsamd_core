0639272ec39afc41e81d982ccc7315bfd0dce3cb
JustBrenkman <JustBrenkman@gmail.com>
2019-11-16 09:35:48 -0700

    init

---
+++ include/startup.h
@@ -0,0 +1,16 @@
+/*
+ * startup.h
+ *
+ * Created: 11/16/2019 9:34:14 AM
+ *  Author: justb
+ */ 
+
+
+#ifndef STARTUP_H_
+#define STARTUP_H_
+
+
+
+
+
+#endif /* STARTUP_H_ */
---
+++ src/startup.c
@@ -0,0 +1,8 @@
+/*
+ * startup.c
+ *
+ * Created: 11/16/2019 9:34:27 AM
+ *  Author: justb
+ */ 
+
+#include "../include/startup.h"
